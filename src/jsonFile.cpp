11d291f98312184db7ad9a0cf06b401d9841d885
lazyloler-create <lazyloler@gmail.com>
2026-09-04 09:38:48 +0200

    Fixed build errors

--- src/jsonFile.cpp
+++ src/jsonFile.cpp
@@ -1,7 +1,6 @@
 #include <fstream>
 #include <iostream>
 #include <nlohmann/json.hpp>
-#include <sstream>
 
 using json = nlohmann::json;
 
@@ -27,7 +26,7 @@ void openJson(json& j, std::string& fName){
 
 std::string loadJson(json &j, std::string fName){
     openJson(j,fName);
-    std::ostringstream buffer;
+    std::string buffer;
     std::ifstream file(fName);
 
     if(!file.is_open()){
@@ -35,7 +34,6 @@ std::string loadJson(json &j, std::string fName){
         return {};
     }
 
-    buffer << file.rdbuf();
     try {
         j = json::parse(buffer);
 
@@ -43,5 +41,5 @@ std::string loadJson(json &j, std::string fName){
         std::cerr << "Json parse error: " << ex.what() << std::endl;
         return {}; //returns empty string
     }
-    return buffer.str();
+    return buffer;
 }
