afc04f799f6d6c32561bd89528c8aa0fca4d3d13
lazyloler-create <lazyloler@gmail.com>
2026-09-21 11:40:16 +0200

    Switched to CMake for SSL client and faster build time

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 **(WIP PROJECT)**
 
-*Compile:*
+*Build and run using CMake:*
 ``
-    g++ main.cpp src/categories_functions.cpp src/jsonFile.cpp src/treenode_class.cpp src/rentBook.cpp src/getBookInfo.cpp -I. -o library_app #-ggdb #optional for debugging with gdb 
+    cmake -B build
+    cmake --build build
+    ./build/myapp   
 ``
 
 *API:*
--- src/getBookInfo.cpp
+++ src/getBookInfo.cpp
@@ -1,14 +1,17 @@
+#define CPPHTTPLIB_OPENSSL_SUPPORT
 #include <nlohmann/json.hpp>
 #include <httplib.h>
 
 using json = nlohmann::json;
 
 //returns a std::string based on query link and the parameter of the API
-std::string getReqInf(std::string query, std::string param){
-    httplib::Client cli(query);
-
-    auto res = cli.Get(query);
+std::string getReqInf(std::string param){
+    httplib::SSLClient cli("openlibrary.org");
+    
+    auto path        = "/search.json?q=" + param;
+    auto res = cli.Get(path);
     std::string info;
+    
     if(res && res->status == 200){
         try{
             auto j = json::parse(res->body);
@@ -20,18 +23,18 @@ std::string getReqInf(std::string query, std::string param){
     return info;
 }
 
-std::string GetTitle(std::string q){
-    return getReqInf(q, "title");
+std::string GetTitle(){
+    return getReqInf("title");
 }
 
-std::string GetAuthor(std::string q){
-    return getReqInf(q, "author");
+std::string GetAuthor(){
+    return getReqInf("author");
 }
 
-std::string GetDate(std::string q){
-    return getReqInf(q, "publish_date");
+std::string GetDate(){
+    return getReqInf("publish_date");
 }
 
-std::string GetISBN(std::string q){
-    return getReqInf(q, "isbn_10");
+std::string GetISBN(){
+    return getReqInf("isbn_10");
 }
